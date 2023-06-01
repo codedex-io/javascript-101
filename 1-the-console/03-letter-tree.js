31c04a0349957ecddabeefff92ddf9bc69cd66de
Dusch4593 <brandondusch@gmail.com>
2023-06-01 07:30:04 -0400

    add JS Ch. 1 solutions

---
+++ 1-the-console/03-letter-tree.js
@@ -0,0 +1,4 @@
+console.log("   a")
+console.log("  b c")
+console.log(" d e f")
+console.log("g h i j") 
---
+++ 1-the-console/initials.js
@@ -0,0 +1,7 @@
+console.log(" SSS   L    ")
+console.log("S   S  L    ")
+console.log("S      L    ")
+console.log(" SSS   L    ")
+console.log("    S  L    ")
+console.log("S   S  L    ")
+console.log(" SSS   LLLLL")
