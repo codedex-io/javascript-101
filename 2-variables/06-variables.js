28d2ac2bd504a51e14b1ea80c06d388c36b90d70
Sonny Li <sonnynomnom@gmail.com>
2023-06-01 02:06:26 -0400

    update

---
+++ 2-variables/06-variables.js
@@ -0,0 +1,14 @@
+// Let & Const 📦
+// Codédex
+
+const userId = 16180339887
+let userName = "Sonny Li"
+let progress = 0.75
+let xp = 60
+let verified = True
+
+console.log(userId)
+console.log(userName)
+console.log(progress)
+console.log(xp)
+console.log(verified)
---
+++ 2-variables/10-quadratic.js
@@ -0,0 +1,12 @@
+# Quadratic Formula 🧮
+# Codédex
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+root1 = (-b + (b*b - 4*a*c)**0.5) / (2*a)
+root2 = (-b - (b*b - 4*a*c)**0.5) / (2*a)
+
+print(root1)
+print(root2)
