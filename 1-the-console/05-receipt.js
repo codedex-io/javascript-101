1a7c64960cf728ba24534a104d0eb4e27604f76d
Sonny Li <sonnynomnom@gmail.com>
2023-06-04 10:37:20 -0400

    Update and rename 05-time-capsule.js to 05-receipt.js

---
+++ 1-the-console/05-receipt.js
@@ -0,0 +1,34 @@
+// Receipt 🧾
+// Codédex
+
+console.log("=================================");
+console.log("           Sushi Yashin          ");
+console.log("        Brooklyn, NY 11215       ");
+console.log("---------------------------------");
+console.log("Server: Taylor                   ");
+console.log("---------------------------------");
+console.log("Order #: 51354            Dine In");
+console.log("Table: B4               Guests: 2");
+console.log("---------------------------------");
+console.log("1 🍣 Omakase 10 pcs         58.00");
+console.log("1 🍱 Bento Box              16.50");
+console.log("1 🥣 Miso Soup               3.75");
+console.log("1 🍶 Hot Sake Small          4.50");
+console.log("1 🍦 Ice Cream (Green Tea)   3.50");
+console.log("                                 ");
+console.log("SUB TOTAL:                  86.25");
+console.log("TAXES:                       7.68");
+console.log("                         ========");
+console.log("TOTAL                      $93.93");
+console.log("٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭");
+console.log("Thank you. See you soon :)       ");
+console.log("18% Gratuity = $16.91            ");
+console.log("20% Gratuity = $18.79            ");
+console.log("22% Gratuity = $20.66            ");
+console.log("                                 ");
+console.log("٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭٭");
+console.log("                                 ");
+console.log("                                 ");
+console.log("                                 ");
+console.log("                                 ");
+console.log("=================================");
