7ae3345a8443e2d1d406bb66e2508ac72578186c
Brandon Dusch <brandon@codedex.io>
2023-07-10 12:28:09 -0400

    Update 17-rock-paper-scissors.js

--- 3-conditionals/17-rock-paper-scissors.js
+++ 3-conditionals/17-rock-paper-scissors.js
@@ -8,32 +8,32 @@
 const player = 3;
 computer = Math.floor(Math.random() * 3)
 
-if (player == 0) {
+if (player === 0) {
   if (computer == 0) {
     console.log("Draw");
-  } else if (computer == 1) {
+  } else if (computer === 1) {
     console.log("The computer won!");
-  } else if (computer == 2) {
+  } else if (computer === 2) {
     console.log("The player won!");
   } else {
     console.log("An error occurred");
   }
-} else if (player == 1) {
-  if (computer == 0) {
+} else if (player === 1) {
+  if (computer === 0) {
     console.log("The player won!");
-  } else if (computer == 1) {
+  } else if (computer === 1) {
     console.log("Draw");
-  } else if (computer == 2) {
+  } else if (computer === 2) {
     console.log("The computer won!");
   } else {
     console.log("An error occurred");
   }
-} else if (player == 2) {
-  if (computer == 0) {
+} else if (player === 2) {
+  if (computer === 0) {
     console.log("The computer won!");
-  } else if (computer == 1) {
+  } else if (computer === 1) {
     console.log("The player won!");
-  } else if (computer == 2) {
+  } else if (computer === 2) {
     console.log("Draw");
   } else {
     console.log("An error occurred");
