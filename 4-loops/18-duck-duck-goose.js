1e27be2bf487afb6b2f0d26d66ffc2b7ee5706e6
Dusch4593 <brandondusch@gmail.com>
2023-09-01 14:21:06 -0400

    Add solutions for JS Ch. 4: Loops

---
+++ 4-loops/18-duck-duck-goose.js
@@ -0,0 +1,11 @@
+// Duck Duck Goose 🪿
+// Codédex
+
+let randomNumber = Math.floor(Math.random() * 10);
+
+while (randomNumber != 7) {
+  console.log("Duck 🦆");
+  randomNumber = Math.floor(Math.random() * 10);
+}
+
+console.log("Goose! 🪿");
---
+++ 4-loops/21-even-the-odds.js
@@ -0,0 +1,13 @@
+// Even The Odds 2️⃣
+// Codédex
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 2 == 1) {
+    continue;
+  } else if (i == 42) {
+    console.log(i);
+    break;
+  } else {
+    console.log(i);
+  }
+}
