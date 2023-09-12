76c19f4d70ecf61bd75e444e4b1ba3890499ed06
Dusch4593 <brandondusch@gmail.com>
2023-09-12 07:47:06 -0400

    rename solution for exercise 20

--- 4-loops/20-i-solemnly-swear.js
+++
@@ -1,8 +0,0 @@
-// I Solemnly Swear 🗺️
-// Codédex
-
-for (let i = 0; i < 100; i++) {
-  console.log("I Solemnly Swear That I Am Up To No Good");
-}
-
-console.log("We Are Proud To Present The World of Programming!");
---
+++ 4-loops/20-must-not-tell-lies.js
@@ -0,0 +1,6 @@
+// Must Not Tell Lies
+// Codédex
+
+for (let i = 0; i < 100; i++) {
+  console.log("I Must Not Tell Lies");
+}
