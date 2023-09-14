8ce33fe48893ac3210960a5472833ceffca581b1
Brandon Dusch <brandon@codedex.io>
2023-09-14 07:21:44 -0400

    Update 19-lucky-number.js

--- 4-loops/19-lucky-number.js
+++ 4-loops/19-lucky-number.js
@@ -6,7 +6,7 @@ let guess = Math.floor(Math.random() * 10) + 1;
 
 while (guess != luckyNumber) {
   console.log(`${guess} is not it.`)
-  let guess = Math.floor(Math.random() * 10) + 1;
+  guess = Math.floor(Math.random() * 10) + 1;
 }
 
 console.log(`You got it! The lucky number was ${luckyNumber}`);
