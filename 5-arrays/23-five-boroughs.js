27a97ba4b3e8fb52e57422cbceb22eb84c2c7f61
Dusch4593 <brandondusch@gmail.com>
2023-09-15 08:40:51 -0400

    add solutions for JS Ch. 5

---
+++ 5-arrays/23-five-boroughs.js
@@ -0,0 +1,12 @@
+// Five Boroughs 🗽
+// Codédex
+
+const myNYCDestinations = [
+  "🗽 Manhattan - Times Square",
+  "🏟️ The Bronx - Yankee Stadium",
+  "🎡 Brooklyn - Coney Island",
+  "✈️ Queens - Astoria Park",
+  "🌉 Staten Island - Historic Richmond Town"
+  ];
+  
+  console.log(myNYCDestinations);
---
+++ 5-arrays/24-grocery-trip.js
@@ -0,0 +1,10 @@
+// Grocery Trip 🛒
+// Codédex
+
+let groceryList = ["🥛 Milk", "🥑 Avocado", "🥚 Eggs ", "🍞 Bread"];
+
+groceryList[2] = "🧈 Butter";
+
+groceryList[4] = "🧼 Laundry Soap"
+
+console.log(groceryList);
