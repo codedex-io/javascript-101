5580e5adca5e3c20b528bb635b8dfd85107f23df
uio23 <98493983+uio23@users.noreply.github.com>
2023-10-15 10:27:32 +1300

    03-letter-tree.js issue
    #hacktoberfest
    
    Add missing semicolons

--- 1-the-console/03-letter-tree.js
+++ 1-the-console/03-letter-tree.js
@@ -1,7 +1,7 @@
 // Letter Tree 🌲
 // Codédex
 
-console.log("   a")
-console.log("  b c")
-console.log(" d e f")
-console.log("g h i j") 
+console.log("   a");
+console.log("  b c");
+console.log(" d e f");
+console.log("g h i j");
