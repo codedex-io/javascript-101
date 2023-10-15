728f5a4489ef392aaf24b9a3f5fc81580daee086
root <root@Urizen.localdomain>
2023-10-15 23:10:28 +0100

    Fixed the input required files with the readline package

--- 2-variables/10-hypotenuse.js
+++ 2-variables/10-hypotenuse.js
@@ -1,10 +1,17 @@
 // Pythagorean Theroem 📐
 // Codédex
 'use strict'
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-let a = int(input("Enter a: "))
-let b = int(input("Enter b: "))
+rl.question("Enter a: ", function (a) {
+    rl.question("Enter b: ", function (b) {
+        const c = Math.sqrt(a * a + b * b);
+        console.log(`The hypotenuse (c) is: ${c}`);
+        rl.close();
+    });
+});
 
-c = (a*a + b*b)**0.5
-
-print(c)
