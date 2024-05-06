7b1eb7d14a0df7aed64a61a8872d165547e565d9
Dusch4593 <brandondusch@gmail.com>
2024-05-06 16:14:47 -0400

    update solution for Ch.6 Ex.32

--- 6-functions/32-clout.js
+++ 6-functions/32-clout.js
@@ -2,8 +2,8 @@
 // Codédex
 
 const recentTikTokViews = [1932, 2300, 453, 5222, 6733, 7402, 8334];
-const recentInstagramViews = [936, 2300, 453, 5222, 6733, 7402, 8334];
-const recentYouTubeViews = [989, 2300, 453, 5222, 6733, 7402, 8334];
+const recentInstagramViews = [936, 2576, 453, 7013, 5489, 7402, 3921];
+const recentYouTubeViews = [2300, 453, 5222, 989, 6733, 7402, 2789];
 
 function mean(viewsArray) {
   let totalViews = 0;
@@ -16,7 +16,7 @@ function mean(viewsArray) {
 }
 
 function median(viewsArray) {
-  const sortedStats = viewsArray.sort();
+  const sortedStats = viewsArray.sort(function(a, b) {return a - b});
   const middleIndex = Math.floor(viewsArray.length / 2);
   
   return sortedStats[middleIndex];
@@ -26,13 +26,16 @@ function median(viewsArray) {
 console.log("TikTok");
 console.log("Mean:", mean(recentTikTokViews));
 console.log("Median:", median(recentTikTokViews));
+console.log();
 
 // Instagram
 console.log("Instagram");
 console.log("Mean:", mean(recentInstagramViews));
 console.log("Median:", median(recentInstagramViews));
+console.log();
 
 // YouTube
 console.log("YouTube");
 console.log("Mean:", mean(recentYouTubeViews));
 console.log("Median:", median(recentYouTubeViews));
+console.log();
