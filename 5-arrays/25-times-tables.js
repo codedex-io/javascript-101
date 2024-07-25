4106684797c3992ecd8777e8ac3666e0ad7f6875
Brandon Dusch <brandon@codedex.io>
2024-07-25 06:14:07 -0400

    Update 25-times-tables.js

--- 5-arrays/25-times-tables.js
+++ 5-arrays/25-times-tables.js
@@ -4,6 +4,6 @@
 const multiple = 5;
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-for (let i = 0; i < numbers.length; i++) {
-  console.log(multiple + " x " + i + " = " + multiple * i);
+for(let i = 0; i < numbers.length; i++){
+  console.log(multiple + " x " + numbers[i] + " = " + multiple * numbers[i])
 }
