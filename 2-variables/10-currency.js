6d190144eaaae2eae7c91ee44d5442648321d080
xin ying <m001720@permatapintar.ukm.edu.my>
2024-10-10 16:10:49 +0800

    Update 10-currency.js

--- 2-variables/10-currency.js
+++ 2-variables/10-currency.js
@@ -1,10 +1,15 @@
 // Currency 💵
 // Codédex
 
-let yuan = 1895.66
-let yen = 872.10
-let won = 20005.50
+let yuan = 1895.66;
+let yen = 872.10;
+let won = 20005.50;
 
-let total = yuan * 0.15 + yen * 0.0077 + won * 0.00080
+let total = yuan * 0.15 + yen * 0.0077 + won * 0.00080;
 
-console.log(total)
+console.log("What do you have left in yuan? ", yuan);
+console.log("What do you have left in yen? ", yen);
+console.log("What do you have left in won? ", won);
+
+console.log();
+console.log(total);
