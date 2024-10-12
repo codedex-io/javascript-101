cd5c6a862b22346df5aa66cdbd8f004a892cda23
Jakub Sova <e-mail@sovajakub.cz>
2024-10-12 02:32:05 +0200

    fix: strict equality

--- 3-conditionals/15-magic-8-ball.js
+++ 3-conditionals/15-magic-8-ball.js
@@ -7,23 +7,23 @@ const randomNumber = Math.floor(Math.random() * 9) + 1
 
 let answer = ""
 
-if (randomNumber == 1){
+if (randomNumber === 1){
   answer = 'Yes - definitely';
-} else if (randomNumber == 2) {
+} else if (randomNumber === 2) {
   answer = 'It is decidedly so'
-} else if (randomNumber == 3) {
+} else if (randomNumber === 3) {
   answer = 'Without a doubt'
-} else if (randomNumber == 4) {
+} else if (randomNumber === 4) {
   answer = 'Reply hazy, try again'
-} else if (randomNumber == 5) {
+} else if (randomNumber === 5) {
   answer = 'Ask again later'
-} else if (randomNumber == 6) {
+} else if (randomNumber === 6) {
   answer = 'Better not tell you now'
-} else if (randomNumber == 7) {
+} else if (randomNumber === 7) {
   answer = 'My sources say no'
-} else if (randomNumber == 8) {
+} else if (randomNumber === 8) {
   answer = 'Outlook not so good'
-} else if (randomNumber == 9) {
+} else if (randomNumber === 9) {
   answer = 'Very doubtful'
 } else {
   answer = 'Error'
