25d5ba7f9a8251353bc9991aaf8387e408a76792
Sonny Li <sonnynomnom@gmail.com>
2024-10-13 14:14:11 -0400

    Update 10-currency.js

--- 2-variables/10-currency.js
+++ 2-variables/10-currency.js
@@ -7,9 +7,4 @@ let won = 20005.50;
 
 let total = yuan * 0.15 + yen * 0.0077 + won * 0.00080;
 
-console.log("What do you have left in yuan? ", yuan);
-console.log("What do you have left in yen? ", yen);
-console.log("What do you have left in won? ", won);
-
-console.log();
 console.log(total);
