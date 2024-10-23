20f942ce8c89bbd1947fd2be75e32df9651a7008
Freida Portillo <freida.portillo@binmatter.com>
2024-10-22 20:06:38 -0600

    refactor: refactor code

--- 8-trifecta/42-mood-ring/script.js
+++ 8-trifecta/42-mood-ring/script.js
@@ -5,22 +5,10 @@ const stone = document.getElementById('stone');
 
 const randomNumber = Math.floor(Math.random() * 10) + 1;
 
-if (randomNumber === 1) {
-  stone.style.backgroundColor = "red";
-} else if (randomNumber === 2) {
-  stone.style.backgroundColor = "orange";
-} else if (randomNumber === 3) {
-  stone.style.backgroundColor = "yellow";
-} else if (randomNumber === 4) {
-  stone.style.backgroundColor = "green";
-} else if (randomNumber === 5) {
-  stone.style.backgroundColor = "blue";
-} else if (randomNumber === 6) {
-  stone.style.backgroundColor = "#4169e1";
-} else if (randomNumber === 7) {
-  stone.style.backgroundColor = "#00008b";
-} else if (randomNumber === 8) {
-  stone.style.backgroundColor = "purple";
-} else {
-  stone.style.backgroundColor = "black";
+const colors = ["red", "orange", "yellow", "green", "blue", "#4169e1", "#00008b", "purple", "black"];
+
+if(randomNumber > 8 || randomNumber < 0){
+  stone.style.backgroundColor = colors[8];
+} else{
+  stone.style.backgroundColor = colors[randomNumber-1];
 }
